0f327bdadcfada859d33cb79ca81f241eb4a5d6d
Michael Bonner <michaelwbonner@gmail.com>
2025-11-26 18:52:20 -0700

    Style a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+      <main className="w-full max-w-3xl py-32 px-16 bg-white sm:items-start">
         <h1 className="text-4xl font-bold">Sourdough Tracker</h1>
+        <p className="text-lg">Track your sourdough bread baking progress</p>
       </main>
     </div>
   );
